09d3cdab0fcfa6efec41494c5e6424278c4a0d8b
Bek <bekhzod.eshkuziev@arvand.tj>
2024-04-26 14:59:16 +0500

    add ShowDialog

--- lib/features/profil/screens/shop-screen/wudgets/shop.dart
+++ lib/features/profil/screens/shop-screen/wudgets/shop.dart
@@ -99,16 +99,28 @@ class _ShopState extends State<Shop> {
         ),
         Row(
           children: [
-            card(),
-            card(),
-            card(),
+            InkWell(
+              onTap: (){
+                showDialogs(context);
+              },
+              child: card(1)),
+            InkWell(
+              onTap: (){
+                showDialogs(context);
+              },
+              child: card(2)),
+              InkWell(
+              onTap: (){
+                showDialogs(context);
+              },
+              child: card(3)),
           ],
         ),
       ],
     );
   }
 
-  Widget card() {
+  Widget card(int index) {
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Stack(
@@ -145,22 +157,30 @@ class _ShopState extends State<Shop> {
                       SizedBox(
                         height: 4,
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 19, 27, 234),
-                            borderRadius: BorderRadius.circular(5)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 4,
-                          ),
-                          child: Text(
-                            "Купить",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 10),
+                      InkWell(
+                        onTap: (){
+                         index == 1 ? showDialogError(context) : showDialogSuccess(context);
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 19, 27, 234),
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 4,
+                            ),
+                            child: Text(
+                              "Купить",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 10),
+                            ),
                           ),
                         ),
+                      ),
+                       SizedBox(
+                        height: 4,
                       ),
                     ],
                   ),
@@ -192,4 +212,148 @@ class _ShopState extends State<Shop> {
       ),
     );
   }
+
+  showDialogs(context){
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+            // title: Text('TOO <<COMPANY>>', style: TextStyle(fontSize: 16),),
+            actions:[ Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('TOO <<COMPANY>>', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                      IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.close),)
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 150,
+                      child: Text('Компания представлает широкий спектр IT-услуг для среднего бизнеса', style: TextStyle(fontWeight: FontWeight.bold),)),
+                Image.asset(
+                  "asset/images/almaty2.jpg",
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
+                  ],
+                )
+                
+              ],
+            ),]
+        )
+    );
+  }
+
+    showDialogError(context){
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+            // title: Text('TOO <<COMPANY>>', style: TextStyle(fontSize: 16),),
+            actions:[ Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                     SizedBox(),
+                      IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.close),)
+                    ],
+                  ),
+                ),
+                
+                Center(
+                  child: Icon(Icons.error, color: Colors.red,),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+               Center(
+                 child: Container(
+                        width: 190,
+                        child: Text('Ошибка у Вас недостаточно коинов', textAlign: TextAlign.center, style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),)),
+               ),
+               
+                
+              ],
+            ),]
+        )
+    );
+  }
+
+  showDialogSuccess(context){
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+            // title: Text('TOO <<COMPANY>>', style: TextStyle(fontSize: 16),),
+            actions:[ Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                     SizedBox(),
+                      IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.close),)
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Icon(Icons.check_circle_outline, color: Colors.green,size: 40,),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+               Center(
+                 child: Container(
+                        width: 190,
+                        child: Text('Ваш заказ #723487 готов', textAlign: TextAlign.center, style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),)),
+               ),
+               Center(
+                 child: Container(
+                        width: 190,
+                        child: Text('Ваш ваучер', textAlign: TextAlign.center, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),)),
+               ),
+               SizedBox(
+                height: 10,
+               ),
+
+               Center(
+                 child: Padding(
+                   padding: const EdgeInsets.symmetric(horizontal: 20),
+                   child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text("QRTWIUW", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                          Icon(Icons.copy, color: Colors.grey,size: 20,),
+                        ],
+                      ),
+                    ),
+                   ),
+                 ),
+               )
+               
+                
+              ],
+            ),]
+        )
+    );
+  }
+
 }
