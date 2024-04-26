26ebb6d1f35f9b09b8965888ce0194312b35bb78
Bek <bekhzod.eshkuziev@arvand.tj>
2024-04-26 09:55:42 +0500

    update place

--- lib/features/home_screen.dart
+++ lib/features/home_screen.dart
@@ -62,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       NewsScreen(),
       const PlayScreen(),
-      const ProfilScreen(),
+      const PlacesScreen(),
       const ProfilScreen(),
     ];
   }
